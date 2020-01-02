37ca06f384d2825518fff3bb18f95a22362f1702
Daniel Corn <info@cundd.net>
2020-01-02 10:13:05 +0100

    Initial Commit

---
+++ chordeditor/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  chordeditor
+//
+//  Created by Daniel Corn on 02.01.20.
+//  Copyright © 2020 Daniel Corn. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ chordeditor/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  chordeditor
+//
+//  Created by Daniel Corn on 02.01.20.
+//  Copyright © 2020 Daniel Corn. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
